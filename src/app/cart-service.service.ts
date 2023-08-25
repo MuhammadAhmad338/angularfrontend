0bb0749f2004dfa27f03681c64deeba3ac7fd1f3
Your Name <you@example.com>
2023-08-26 01:27:02 +0500

    latest and updated commit

--- src/app/cart-service.service.ts
+++ src/app/cart-service.service.ts
@@ -13,6 +13,7 @@ export class CartServiceService {
   public products = new BehaviorSubject<any>([]);
   public productList = new BehaviorSubject<any>([]);
   constructor(private http: HttpClient) {}
+
   addProduct(product: Product): void {
     const productExistsinCart = this.cartProductList.find(({name}) => name === product.name);
     if (!productExistsinCart) {
@@ -23,29 +24,31 @@ export class CartServiceService {
       console.log("Product Exists!");
     }
   }
+
   removeFromCart(product: Product): void {
     const productExistsinCart = this.cartProductList.find(({name}) => name === product.name);
     if (productExistsinCart) {
       this.cartProductList = this.cartProductList.filter(({name}) => name !== product.name);
       this.productList.next(this.cartProductList);
+      console.log("Product is removed!");
     } else {
       console.log("No Product Exists!");
     }
   }
+
   clearThecart(): void {
     this.cartProductList = [];
     this.productList.next(this.cartProductList);
   }
+
   getCartProducts(): any {
     return this.productList.asObservable();
   }
+
   getProductsFromInternet() {
     const data = this.http.get(this.url);
     data.forEach((mydata: any) => this.products.next(mydata));
     return this.products.asObservable();
-  }
-  searchTheProducts() {
-    this.http.get(this.url);
-  }
+  } 
 
 }
